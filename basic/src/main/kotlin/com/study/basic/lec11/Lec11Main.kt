6b1e922b9e395a4337a794c4cf7d5baccbc0b6c4
parkje0927 <parkje09270@gmail.com>
2023-07-28 23:58:45 +0900

    feat: 11 내용 추가 - 코틀린에서 접근 제어를 다루는 방법

--- basic/src/main/kotlin/com/study/basic/lec11/Lec11Main.kt
+++ basic/src/main/kotlin/com/study/basic/lec11/Lec11Main.kt
@@ -11,6 +11,8 @@ fun main() {
      * private : 선언된 클래스내에서만 접근 가능
      *
      * 생성자는 접근제어자를 붙이려면 constructor 를 써줘야한다.
+     * 자바에서는 코틀린 모듈의 internal 코드를 가져올 수 있다.
+     * 프로퍼티의 custom setter 에 접근 지시어를 붙일 수 있다.
      */
 }
 
