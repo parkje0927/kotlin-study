cb580457db5a6a9f8a2df0e83ccdb89cf97d80dc
parkje0927 <parkje09270@gmail.com>
2023-07-28 23:56:37 +0900

    feat: 11 내용 추가 - 코틀린에서 접근 제어를 다루는 방법

---
+++ basic/src/main/kotlin/com/study/basic/lec11/Lec11Main.kt
@@ -0,0 +1,19 @@
+package com.study.basic.lec11
+
+fun main() {
+
+    /**
+     * 접근제어자
+     *
+     * public => 기본 접근 제어자
+     * protected : **선언된 클래스** 또는 하위 클래스에서만 접근 가능
+     * internal : **같은 모듈**에서만 접근 가능
+     * private : 선언된 클래스내에서만 접근 가능
+     *
+     * 생성자는 접근제어자를 붙이려면 constructor 를 써줘야한다.
+     */
+}
+
+class Cat protected constructor(
+
+)
---
+++ basic/src/main/kotlin/com/study/basic/lec11/StringUtils.kt
@@ -0,0 +1,5 @@
+package com.study.basic.lec11
+
+fun isDirectoryPath(path: String): Boolean {
+    return path.endsWith("/")
+}
