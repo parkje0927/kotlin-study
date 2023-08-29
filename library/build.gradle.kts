fa546d38842f54537dcdbfece7c2c1cc1f62f136
parkje0927 <parkje09270@gmail.com>
2023-08-29 22:32:28 +0900

    build: 의존성 추가 및 yml 파일 설정 추가

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
