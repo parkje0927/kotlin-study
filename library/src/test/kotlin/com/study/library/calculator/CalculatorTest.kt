760a28a67741fa2a6286fd91930fe3e7d7482bcf
parkje0927 <parkje09270@gmail.com>
2023-08-31 20:04:38 +0900

    test: 리팩토링 하기 전 테스트 코드 추가

--- library/src/test/kotlin/com/study/library/calculator/CalculatorTest.kt
+++ library/src/test/kotlin/com/study/library/calculator/CalculatorTest.kt
@@ -1,8 +1,13 @@
 package com.study.library.calculator
 
+import org.junit.jupiter.api.Test
+
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
 }
 
 class CalculatorTest {
@@ -18,15 +23,83 @@ class CalculatorTest {
      * => 2번
      */
     fun addTest() {
+        //given
         val calculator = Calculator(5)
+
+        //when
         calculator.add(3)
 
-        val expectedCalculator = Calculator(8)
+        //then
+//        val expectedCalculator = Calculator(8)
 //        if (calculator != expectedCalculator) {
 //            throw IllegalStateException()
 //        }
+
         if (calculator.number != 8) {
             throw IllegalStateException()
         }
+    }
+
+    fun minusTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.minus(3)
+
+        //then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun multiplyTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.multiply(3)
+
+        //then
+        if (calculator.number != 15) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun divideTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.divide(2)
+
+        //then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    @Test
+    fun divideWithZeroTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            if (e.message != "0으로 나눌 수 없습니다.") {
+                throw IllegalStateException("예외 메세지가 다릅니다.")
+            }
+            //success
+            return
+        } catch (e: Exception) {
+            //fail
+            throw IllegalStateException()
+        }
+        throw IllegalStateException("기대하는 예외가 발생하지 않았다.")
+
+        //then
+
     }
 }
