5568446ce9fb171b26c7a8bbeaf902e3ddeafded
parkje0927 <parkje09270@gmail.com>
2023-09-11 22:10:59 +0900

    feat: 확장함수 및 공통함수를 정의한 ExceptionUtils 파일 추가

---
+++ library/src/main/kotlin/com/study/library/util/ExceptionUtils.kt
@@ -0,0 +1,13 @@
+package com.study.library.util
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.findByIdOrNull
+
+fun fail(): Nothing {
+    throw IllegalArgumentException()
+}
+
+fun <T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T {
+    //this = CrudRepository
+    return this.findByIdOrNull(id) ?: fail()
+}
